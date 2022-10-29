6fdbe8d0bbf13498ea4fac39101c3ac2e3503cc7
NBADRAUN <Nbadraun@gmail.com>
2022-10-29 12:44:08 -0600

    Final Edits to Script.JS and Readmd.md

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # Create_A_Password
-Password Generator Application
+
+## Description
+
+This is a Random Password Generator Application.  It provides a method to provide a user to generate their own random password to use when registering on other websites to ensure the highest level of password security and to avoid being hacked by using the same password on multiple websites or a password that is more familiar and can be guessed.  
+
+## Installation
+
+There is no instalation required.  This application runs on a webpage and can be accessed by going to https://nbadraun.github.io/Create_A_Password/.  
+
+When using this application, no user data is saved once you have closed the application.  
+
+
+## Usage
+
+1.  go to https://nbadraun.github.io/Create_A_Password/
+2.  Click the red button "Generate Password".  
+3.  Follow the alert prompts at the top of your screen to select a password based on your preferences.  
+4.  Once the password is generated, it will be shown in the box above the Generate Password button. 
+5.  Highlight the password, then copy to use on other applications or to store elsewhere for later use.  
+
+
+## License
+MIT License / Copyright (c) 2022 NBADRAUN <br>
+Details can be found by going to https://github.com/NBADRAUN/Create_A_Password/blob/main/LICENSE
+
+## User Story: 
+AS AN employee with access to sensitive data <br>
+I WANT to randomly generate a password that meets certain criteria <br>
+SO THAT I can create a strong password that provides greater security 
+
+## Acceptance Criteria: 
+GIVEN I need a new, secure password <br>
+WHEN I click the button to generate a password <br>
+THEN I am presented with a series of prompts for password criteria <br>
+WHEN prompted for password criteria <br>
+THEN I select which criteria to include in the password <br>
+WHEN prompted for the length of the password <br>
+THEN I choose a length of at least 8 characters and no more than 128 characters <br>
+WHEN asked for character types to include in the password <br>
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters <br>
+WHEN I answer each prompt <br>
+THEN my input should be validated and at least one character type should be selected <br>
+WHEN all prompts are answered <br>
+THEN a password is generated that matches the selected criteria <br>
+WHEN the password is generated <br>
+THEN the password is either displayed in an alert or written to the page 
+
+## Screenshots of Application
+<table>
+    <tr>
+        <td valign="top">Starting Page: <br><img src="assets\Images\Create_A_Password.png" width="300px" height="400px" alt="Picture of the page before anything has been entered."> </td>
+        <td valign="top">Character Length Prompt:<br><img src="assets\Images\Length_Prompt.png" width="300px" height="400px" alt="Picture of the page for the user to enter the desired password length."> </td>
+        <td valign="top">Lower case Character Prompt:<br><img src="assets\Images\LowerCase_Prompt.png" width="300px" height="400px" alt="Picture of the page for the user to enter the if they want to include lowercase letters."> 
+    </tr>
+<table>
+        <td valign="top">Number Prompt:<br><img src="assets\Images\Numbers_Prompt.png" width="300px" height="400px" alt="Picture of the page for the user to enter the if they want to include numbers."> </td>
+        <td valign="top">Special Character Prompt:<br><img src="assets\Images\Special_Prompt.png" width="300px" height="400px" alt="Picture of the page for the user to enter the if they want to include special characters."> </td>
+        <td valign="top">No Characters Selected Prompt:<br><img src="assets\Images\No_Characters_Selected_Error.png" width="300px" height="400px" alt="Picture of the page the user will get if they did not select any character options."> </td>
+</tr>
+<table> 
+        <td valign="top">Final Output:<br><img src="assets\Images\Final_Output.png" width="300px" height="400px" alt="Picture of the page for the user will see after they have answered all the prompts and the password has be returned into the text box."> </td>
+
+
--- script.js
+++ script.js
@@ -4,133 +4,127 @@ var selections = [];
 var LengthInput = 0;  /// Sets the inital password length to 0  
 var PasswordCharacterOptions = [];  /// builds the selected charicter array with 0 elements
 var RandomPasswordCharacterOptions = ShuffledAndTrimmedPassword();  ///// This passes the final character string to display //////
- 
 
-  
+
+
 
 ///This generates the password///////
 function generatePassword() { 
     PasswordCharacterOptions = [] 
-    var LengthOutput = PWLengthPrompt(); 
-    var LowerLettersOutput = LowerLettersPrompt();  
-    var UpperLettersOutput = UpperLettersPrompt(); 
-    var NumbersOutput = NumbersPrompt(); 
-    var SpecialOutput = SpecialPrompt();    
-    var TotalSelections = PickOne();  
-    var AllInString = ShuffledAndTrimmedPassword();    
-    return AllInString; 
+    var LengthOutput = PWLengthPrompt();   //function to determine lenght of password user wants //
+    var LowerLettersOutput = LowerLettersPrompt();  //function to determine if user wants to include lowercase letters //
+    var UpperLettersOutput = UpperLettersPrompt();  //function to determine if user wants to include uppercase letters //
+    var NumbersOutput = NumbersPrompt();   //function to determine if user wants to include numbers //
+    var SpecialOutput = SpecialPrompt();   //function to determine if user wants to include special characters // 
+    var TotalSelections = PickOne();    //function to determine if user selected at least one character set //
+    var AllInString = ShuffledAndTrimmedPassword();    //function to randomize and then trim the password to the users selected length //
+    return AllInString;  //returns the selected password to display on the page //
 }
 
 
 /////// All the functions needed to populate the array are listed below /////// 
 
-
     ///This is the PW length prompt //////
-    function PWLengthPrompt(){
+function PWLengthPrompt(){
     while ((LengthInput < 8) || (LengthInput > 128)) {
         LengthInput = parseInt(window.prompt("Choose a length between 8 and 128 characters"));
             if(isNaN(LengthInput)) {
                 LengthInput=0; 
-            }
         }
-            console.log(LengthInput);                 
+    }
 }
 
-
     ///This is the lower letters prompt /////
-    function LowerLettersPrompt(){
-    var LowerLettersSet = ["a","b","c" ,"d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+function LowerLettersPrompt(){
+    var LowerLettersSet = ["a","b","c" ,"d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+                          ,"a","b","c" ,"d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 
+                          ,"a","b","c" ,"d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+                          ,"a","b","c" ,"d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+                          ,"a","b","c" ,"d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
     var LowerLettersInput = window.prompt("Do you want lower case letters? Y or N"); 
         if (LowerLettersInput==="Y"||LowerLettersInput==="y"){
             PasswordCharacterOptions=PasswordCharacterOptions.concat(LowerLettersSet); 
-            console.log(PasswordCharacterOptions); 
             selections = selections.concat("LowerLetterSet"); 
-            console.log(selections); 
-            console.log(selections.length); 
         } else {      
-            console.log(selections.length); 
-        }
+    }
  }
 
     ///This is the upper letters prompt /////
-    function UpperLettersPrompt(){
-        var UpperLettersSet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-        var UpperLettersInput = window.prompt("Do you want upper case letters? Y or N"); 
-            if (UpperLettersInput==="Y"||UpperLettersInput==="y"){
+function UpperLettersPrompt(){
+    var UpperLettersSet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+                          ,"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+                          ,"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+                          ,"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+                          ,"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var UpperLettersInput = window.prompt("Do you want upper case letters? Y or N"); 
+        if (UpperLettersInput==="Y"||UpperLettersInput==="y"){
                 PasswordCharacterOptions=PasswordCharacterOptions.concat(UpperLettersSet); 
-                console.log(PasswordCharacterOptions); 
-                console.log(UpperLettersInput); 
                 selections = selections.concat("UpperLetterSet"); 
-                console.log(selections); 
-                console.log(selections.length); 
-            } else {     
-                console.log(selections.length);  
-            }
-     }
+        } else {     
+    }
+}
 
     ///This is the numbers prompt /////
-    function NumbersPrompt(){
-        var NumbersSet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-        var NumbersInput = window.prompt("Do you want numbers? Y or N"); 
-            if (NumbersInput==="Y"||NumbersInput==="y"){
+function NumbersPrompt(){
+    var NumbersSet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9","0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+                     ,"0", "1", "2", "3", "4", "5", "6", "7", "8", "9","0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+                     ,"0", "1", "2", "3", "4", "5", "6", "7", "8", "9","0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+                     ,"0", "1", "2", "3", "4", "5", "6", "7", "8", "9","0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+                     ,"0", "1", "2", "3", "4", "5", "6", "7", "8", "9","0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+                     ,"0", "1", "2", "3", "4", "5", "6", "7", "8", "9","0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+                     ,"0", "1", "2", "3", "4", "5", "6", "7", "8", "9","0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    var NumbersInput = window.prompt("Do you want numbers? Y or N"); 
+        if (NumbersInput==="Y"||NumbersInput==="y"){
                 PasswordCharacterOptions=PasswordCharacterOptions.concat(NumbersSet); 
-                console.log(PasswordCharacterOptions); 
                 selections = selections.concat("NumberSet"); 
-                console.log(selections); 
-                console.log(selections.length); 
-            } else {    
-                console.log(selections.length);   
-            }
-     }
+        } else {    
+    }
+}
 
-    ///This is the special charicter prompt /////
-    function SpecialPrompt(){
-        var SpecialSet = ["!","'","#","$","%","&","'",")","(","*","+","-",".","/",":",";","<","=",">","?","@","[","^","~",];
-        var SpecialInput = window.prompt("Do you want special charicters? Y or N"); 
-            if (SpecialInput==="Y"||SpecialInput==="y"){
+    ///This is the special character prompt /////
+function SpecialPrompt(){
+    var SpecialSet = ["!","'","#","$","%","&","'",")","(","*","+","-",".","/",":",";","<","=",">","?","@","[","^","~","!","'","#","$","%","&","'",")","(","*","+","-",".","/",":",";","<","=",">","?","@","[","^","~"
+                     ,"!","'","#","$","%","&","'",")","(","*","+","-",".","/",":",";","<","=",">","?","@","[","^","~","!","'","#","$","%","&","'",")","(","*","+","-",".","/",":",";","<","=",">","?","@","[","^","~"
+                     ,"!","'","#","$","%","&","'",")","(","*","+","-",".","/",":",";","<","=",">","?","@","[","^","~","!","'","#","$","%","&","'",")","(","*","+","-",".","/",":",";","<","=",">","?","@","[","^","~"];
+    var SpecialInput = window.prompt("Do you want special characters? Y or N"); 
+        if (SpecialInput==="Y"||SpecialInput==="y"){
                 PasswordCharacterOptions=PasswordCharacterOptions.concat(SpecialSet); 
-                console.log(PasswordCharacterOptions); 
                 selections = selections.concat("SpecialSet"); 
-                console.log(selections); 
-                console.log(selections.length); 
-            } else {     
-                console.log(selections.length);  
-            }
-     }
+        } else {     
+    }
+}
     
-
-     function PickOne(){
+    ///This makes sure the user selected at least one character set.  if not, start over///
+function PickOne(){
         if ((selections.length<1)){
             window.prompt("You must select at least one character set.  Please click ok or press enter to start over");
         LengthInput=0;
         PasswordCharacterOptions = []; 
             ShuffledAndTrimmedPassword();  
         } else{ 
-        }
-     }
-
+    }
+}
 
     //// This shuffles the characters in the PasswordCharacterOptions array, ///////
-    function ShuffledPasswordCharacterOptions(){
-        var shuffled = PasswordCharacterOptions.sort(function(){return 0.5 - Math.random()});
+function ShuffledPasswordCharacterOptions(){
+    var shuffled = PasswordCharacterOptions.sort(function(){return 0.5 - Math.random()});
         return shuffled; 
-    }
+}
 
     /// This trims the PW to length then joins the array elements //////////
-    function ShuffledAndTrimmedPassword(){
-        var ShuffledAndTrimmed = ShuffledPasswordCharacterOptions().slice(0,LengthInput);  
-        var joined = ShuffledAndTrimmed.join(""); 
+function ShuffledAndTrimmedPassword(){
+    var ShuffledAndTrimmed = ShuffledPasswordCharacterOptions().slice(0,LengthInput);  
+    var joined = ShuffledAndTrimmed.join(""); 
         LengthInput=0; 
         selections=[]; 
-        return joined; 
-        
-    }
+        return joined;       
+}
    
-    
-    // // Assignment code here
+   
+    // Assignment code here This was the original code provided // 
 
     // // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+    var generateBtn = document.querySelector("#generate");
 
     // Write password to the #password input
 function writePassword() {
@@ -138,6 +132,6 @@ function writePassword() {
    var passwordText = document.querySelector("#password");
    passwordText.value = password;
 }
-    // // Add event listener to generate button
+    // Add event listener to generate button
     generateBtn.addEventListener("click", writePassword); 
 
